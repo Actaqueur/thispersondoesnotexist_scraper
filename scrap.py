ca82f40bb58133d6a6f483cfca5a04f6c9f8ac1b
0xUserPaword <118621236+0xUserPaword@users.noreply.github.com>
2022-11-19 17:06:19 +0100

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -1,11 +1,6 @@
-# scrap visage with https://www.thispersondoesnotexist.com/
-
 import requests
 import os
 
-
-
-# scrap 500 visage to save in a folder
 def scrap():
     for i in range(500):
         r = requests.get('https://thispersondoesnotexist.com/image')
